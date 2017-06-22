6ee3768431121bc8779a536d6b4de8673918dfa1
hexa <i.pylypenko@hexa.com.ua>
2017-06-22 12:09:42 +0300

    Change i18n configuration

--- src/config/main.php
+++ src/config/main.php
@@ -3,19 +3,20 @@
 return [
     'id'                  => 'support',
     'controllerNamespace' => 'hexa\yiisupport\controllers',
-//    'components'          => [
-//        'i18n'         => [
-//            'class'        => '\yii\i18n\I18N',
-//            'translations' => [
-//                '*' => [
-//                    'class'    => 'yii\i18n\PhpMessageSource',
-//                    'basePath' => '@yiisupport/messages',
-//                ],
-//            ],
-//        ],
-//    ],
+    'components'          => [
+        'i18n' => [
+            'class'        => '\yii\i18n\I18N',
+            'translations' => [
+                '*' => [
+                    'class'          => 'yii\i18n\PhpMessageSource',
+                    'basePath'       => __DIR__ . '/messages',
+                    'sourceLanguage' => 'en-US'
+                ],
+            ],
+        ],
+    ],
     'params'              => [
-        'secret'    => 'comment',
+        'secret' => 'comment',
     ],
     'adminRole'           => 'admin',
     'userRole'            => 'user',
