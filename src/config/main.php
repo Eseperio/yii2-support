7ae30d78e5fd58b4c7d684897ff6f24e4bc8172a
hexa <i.pylypenko@hexa.com.ua>
2017-06-22 12:10:38 +0300

    Change i18n configuration

--- src/config/main.php
+++ src/config/main.php
@@ -7,7 +7,7 @@ return [
         'i18n' => [
             'class'        => '\yii\i18n\I18N',
             'translations' => [
-                '*' => [
+                'support*' => [
                     'class'          => 'yii\i18n\PhpMessageSource',
                     'basePath'       => __DIR__ . '/messages',
                     'sourceLanguage' => 'en-US'
