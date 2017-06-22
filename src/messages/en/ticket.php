7fc6e65f33df892a55323440275f9768ab23dbf1
hexa <i.pylypenko@hexa.com.ua>
2017-06-22 12:03:50 +0300

    Add translation messages

--- src/messages/en/ticket.php
+++ src/messages/en/ticket.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Translation map for EN_TICKET
- * @version     1.0
- * @license     http://mit-license.org/
- * @author      Tapakan https://github.com/Tapakan
- * @coder       Alexander Oganov <t_tapak@yahoo.com>
- * @copyright   Copyright (C Hexa,  All rights reserved.
- */
 
 return [
     'Author'       => 'Author',
